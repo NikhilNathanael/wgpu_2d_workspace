def96e87ff7b7377bb627118b9413c0d46bf4c29
NikhilNathanael <nikhilnathanael2020@gmail.com>
2025-03-20 00:19:11 +0530

    restructured code to use a workspace to allow for derive macro usage

---
+++ derive/src/lib.rs
@@ -0,0 +1,19 @@
+// pub fn add(left: u64, right: u64) -> u64 {
+//     left + right
+// }
+
+pub fn vertex_buffer_data (data: proc_macro2::TokenStream) -> proc_macro2::TokenStream {
+	
+	todo!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
